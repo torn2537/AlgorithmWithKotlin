88e9b1cb9c78ccefd5aa322f0f4515c45fbf7ac9
Chupraphawan <Supakorn.Chupraphawan@thomsonreuters.com>
2020-06-17 00:19:07 +0700

    - added Graph and MaximumSubArray class.

--- src/Graph.kt
+++ src/Graph.kt
@@ -68,18 +68,24 @@ class Graph(var numOfNodes: Int, var directed: Boolean, var weighted: Boolean) {
 
 
 fun main() {
-    Graph(5, directed = false, weighted = true).also { graph ->
-        run {
-            graph.addEdge(0, 2, 19)
-            graph.addEdge(0, 3, -2)
-            graph.addEdge(0, 3, -2)
-            graph.addEdge(1, 3) // The default weight is 0 if weighted == true
-            graph.addEdge(1, 4)
-            graph.addEdge(2, 3)
-            graph.addEdge(3, 4)
-            graph.printMatrix()
-            graph.printEdge()
-        }
-    }
+    val graph = Graph(5, directed = false, weighted = true)
+    graph.addEdge(0, 2, 19)
+    graph.addEdge(0, 3, -2)
+    graph.addEdge(0, 3, -2)
+    graph.addEdge(1, 3) // The default weight is 0 if weighted == true
+    graph.addEdge(1, 4)
+    graph.addEdge(2, 3)
+    graph.addEdge(3, 4)
+    graph.printMatrix()
+    graph.printEdge()
+    println("Has edge?: ${graph.hasEdge(3, 4)}")
+
+    val items: IntArray = intArrayOf(1, 2, 3, 4, 5)
+    val joinedToString = items.fold("Elements:", { acc, i -> "$acc $i" })
+
+    println("joinedToString: $joinedToString")
+    val product = items.fold(1, Int::times)
+    println("product: $product")
+
 }
 
---
+++ src/MaximumSubArray.kt
@@ -0,0 +1,60 @@
+class MaximumSubArray {
+    fun maxSumSubArray(arr: Array<Int>, low: Int, high: Int): Int {
+        if (high == low) {
+            return arr[high]
+        }
+        val mid: Int = low + (high - low) / 2
+        val maximumSubLeftArray: Int = maxSumSubArray(arr, low, mid)
+        val maximumSubRightArray: Int = maxSumSubArray(arr, mid + 1, high)
+        val maximumCrossingSubArray: Int = maxCrossingSubArray(arr, low, mid, high)
+
+        return findMaximum(maximumSubLeftArray, maximumSubRightArray, maximumCrossingSubArray)
+    }
+
+    fun maxCrossingSubArray(arr: Array<Int>, low: Int, mid: Int, high: Int): Int {
+        var leftSum: Int = Int.MIN_VALUE
+        var rightSum: Int = Int.MIN_VALUE
+        /*'''
+            iterating from middle
+            element to the lowest element
+            to find the maximum sum of the left
+            subarray containing the middle
+            element also.
+  '     */
+        var i: Int = mid
+        while (i >= low) {
+            sum += arr[i]
+            if (sum > leftSum) {
+                leftSum = sum
+            }
+            i--
+        }
+
+        var sum = 0
+        i = mid + 1
+        while (i <= high) {
+            sum += arr[i]
+            if (sum > rightSum) {
+                rightSum = sum
+            }
+            i++
+        }
+        return (leftSum + rightSum)
+    }
+}
+
+fun findMaximum(num1: Int, num2: Int, num3: Int): Int {
+    return if (num1 > num2 && num1 > num3) {
+        num1
+    } else if (num2 > num1 && num2 > num3) {
+        num2
+    } else {
+        num3
+    }
+}
+
+fun main() {
+    val arr: Array<Int> = arrayOf(3, -1, -1, 10, -3, -2, 4)
+    val result: Int = MaximumSubArray().maxSumSubArray(arr, 0, arr.size - 1)
+    println("Maximum sub array is: $result")
+}
